00f73629b7772c8adde18287b74330744ec8c120
poserko <kokolin2204@gmail.com>
2023-02-15 08:49:56 +0200

    Zad Spisuk

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zad_Spisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
